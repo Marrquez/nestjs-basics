6525f2d55f19715190a81b1b5e9ad4d2dd89d1e4
Cristian Marquez <cristian_marquez@epam.com>
2024-01-29 16:50:27 +0100

    creating a basic CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { Product } from './products.model';
 
 // this controller handle request to: your-domain.com/products
 @Controller('products')
@@ -21,7 +22,32 @@ export class ProductsController {
 
     // This method will be triggered after: /products/products-list
     @Get('products-list')
-    getProducts(): void {
-        
+    getAllProducts() {
+        return this.productsService.getAllProducts();
+    }
+
+    // This method will be triggered after: /products/products-list
+    @Get('product/:title')
+    getProduct(
+        @Param('title') prodId: string
+    ) {
+        return this.productsService.getProduct(prodId);
+    }
+
+    @Patch(':id')
+    updateProduct(
+        @Param('id') prodId: string,
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number,
+    ) {
+        this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+        return null;
+    }
+
+    @Delete(':id')
+    removeProduct(@Param('id') prodId: string,) {
+        this.productsService.deleteProduct(prodId);
+        return null;
     }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './products.model';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ProductsService {
-    products: Product[] = [];
+    private products: Product[] = [];
 
     insertProduct(title: string, description: string, price: number): string {
         const id = uuidv4();
@@ -12,5 +12,53 @@ export class ProductsService {
         this.products.push(newProduct);
 
         return id;
+    }
+
+    getAllProducts(): Product[] {
+        return [...this.products];
+    }
+
+    getProduct(title: string): Product {
+        const product = this.products.find((item) => item.title === title);
+
+        if(!product) {
+            throw new NotFoundException();
+        }
+
+        return {...product};
+    }
+
+    updateProduct(id: string, title: string, description: string, price: number): void {
+        const productIndex = this.products.findIndex((item) => item.id === id);
+        const product = this.products[productIndex];
+
+        if(!product) {
+            throw new NotFoundException();
+        }
+
+        if (title) {
+            product.title = title;
+        }
+
+        if (description) {
+            product.description = description;
+        }
+
+        if (price) {
+            product.price = price;
+        }
+
+        this.products[productIndex] = product;
+    }
+
+    deleteProduct(id: string) {
+        const productIndex = this.products.findIndex((item) => item.id === id);
+        const product = this.products[productIndex];
+
+        if(!product) {
+            throw new NotFoundException();
+        }
+
+        this.products.splice(productIndex, 1);
     }
 }
